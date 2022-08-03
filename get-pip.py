c60e808cce559fb3e64bfc3e0d5e9d83455f65a4
Anderson Tribuzy <andersonaraujo1@msn.com>
2022-08-03 15:52:40 -0300

    Add files via upload

---
+++ get-pip.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import streamlit as st
+from PIL import Image
+
+
+st.title('Jornada Além dos Dados')
+st.text('Aplicação que realiza análise de sentimento dos candidatos à presidência usando dados do Twitter')
+
+
+df = pd.read_csv('Conjunto de Dados\dataframe_final.csv')
+candidato_unico = sorted(df['Candidato'].unique())
+selecionar_candidato = st.sidebar.multiselect('Candidato', candidato_unico, candidato_unico)
+
+df_candidato_selecionado = df[(df['Candidato'].isin(selecionar_candidato))]
+
+st.dataframe(df_candidato_selecionado)
+
+
