deb75e1f2962c9f31754e84fedd4da2466e5172e
Anderson Tribuzy <andersonaraujo1@msn.com>
2022-08-04 11:20:01 -0300

    Add files via upload

--- get-pip.py
+++ get-pip.py
@@ -13,7 +13,7 @@ candidato_unico = sorted(df['Candidato'].unique())
 selecionar_candidato = st.sidebar.multiselect('Candidato', candidato_unico, candidato_unico)
 
 df_candidato_selecionado = df[(df['Candidato'].isin(selecionar_candidato))]
-st.write('Conjunto de Dados ' + str(df_candidato_selecionado.shape[0]) + 'linhas e ' + str(df_candidato_selecionado.len[0]))
+st.write('Conjunto de Dados ' + str(df_candidato_selecionado.shape[0]) + 'linhas')  ##+ str(df_candidato_selecionado.len[0]))
 
 
 st.dataframe(df_candidato_selecionado)
