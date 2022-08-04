fc13bf90f9c5672ceccd6915aa7d74f97bf64791
Anderson Tribuzy <andersonaraujo1@msn.com>
2022-08-04 11:26:33 -0300

    Add files via upload

--- get-pip.py
+++ get-pip.py
@@ -2,20 +2,24 @@ import pandas as pd
 import streamlit as st
 from PIL import Image
 
-
+# Título
 st.title('Jornada Além dos Dados')
+# Sub-título
 st.text('Aplicação que realiza análise de sentimento dos candidatos à presidência usando dados do Twitter')
 
-
+# Leitura do dataset
 df = pd.read_csv('dataframe_final.csv')
 
+# Ordenar valores únicos
 candidato_unico = sorted(df['Candidato'].unique())
+# Criar caixa de seleção com os nomes dos candidatos
 selecionar_candidato = st.sidebar.multiselect('Candidato', candidato_unico, candidato_unico)
-
 df_candidato_selecionado = df[(df['Candidato'].isin(selecionar_candidato))]
-st.write('Conjunto de Dados ' + str(df_candidato_selecionado.shape[0]) + ' linhas')  ##+ str(df_candidato_selecionado.len[0]))
 
+# Título informativo - Dataset
+st.write('Conjunto de Dados possui ' + str(df_candidato_selecionado.shape[0]) + ' linhas')  ##+ str(df_candidato_selecionado.len[0]))
 
+## Visualizar dataset
 st.dataframe(df_candidato_selecionado)
 
 
